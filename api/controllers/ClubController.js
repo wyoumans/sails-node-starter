0fdbb8d287e850dffc9d50027008da66df2b274e
William Youmans <will@williamyoumans.com>
2015-04-09 09:58:24 -0400

    added basic api for clubs and companies

---
+++ api/controllers/ClubController.js
@@ -0,0 +1,11 @@
+/**
+ * ClubController
+ *
+ * @description :: Server-side logic for managing clubs
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
